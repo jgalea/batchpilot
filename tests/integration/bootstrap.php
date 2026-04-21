4ec1c4c79583595b152084089b0a2e87583145e1
Jean Galea <1651502+jgalea@users.noreply.github.com>
2026-04-21 18:36:55 +0200

    test: add wp-env + wp-phpunit integration test harness with plugin boot coverage

---
+++ tests/integration/bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+require __DIR__ . '/../../vendor/autoload.php';
+
+$wp_phpunit_dir = getenv( 'WP_TESTS_DIR' );
+if ( false === $wp_phpunit_dir ) {
+	$wp_phpunit_dir = getenv( 'WP_PHPUNIT__DIR' );
+}
+if ( false === $wp_phpunit_dir ) {
+	$wp_phpunit_dir = __DIR__ . '/../../vendor/wp-phpunit/wp-phpunit';
+}
+
+$_tests_dir = rtrim( $wp_phpunit_dir, '/\\' );
+
+$GLOBALS['wp_tests_options'] = [
+	'active_plugins' => [ 'content-ops/content-ops.php' ],
+];
+
+require_once $_tests_dir . '/includes/functions.php';
+
+tests_add_filter(
+	'muplugins_loaded',
+	static function (): void {
+		require dirname( __DIR__, 2 ) . '/content-ops.php';
+	}
+);
+
+require $_tests_dir . '/includes/bootstrap.php';
